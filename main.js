7f4a1fe7fc78c803dba42d8da2c80321e7c22db6
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-04-29 12:38:25 +0300

    Updated

--- main.js
+++ main.js
@@ -20,3 +20,75 @@ function getMin(arr) {
   let sorted = arr.sort((a, b) => a - b)
   return sorted[0]
 }
+
+class Process {
+  static toJSON (arg1){
+    let jsonString = JSON.stringify(arg1);
+    return jsonString;
+  }
+
+  static fromJSON (jsonString){
+    let arg1 = JSON.parse(jsonString);
+    return arg1;
+  }
+}
+
+// let result = Process.toJSON(dataArr);
+// console.log(result);
+
+// let answer = Process.fromJSON(result);
+// console.log(answer);
+
+function converter(data) {
+  let parsed= Process.fromJSON(data);
+  console.log(parsed);
+}
+
+function callFunc1(arg2, callback1) {
+  let str=Process.toJSON(arg2);
+  callback1(str);
+}
+
+callFunc1(dataArr, converter);
+
+function double(el) {
+  return el * 2;
+}
+
+function doubler(arg3) {
+  let parsed= Process.fromJSON(arg3);
+  let doubled= parsed.map(double);
+  console.log(doubled);
+}
+
+function onlyEven(arg4) {
+  let parsed= Process.fromJSON(arg4);
+  let result= parsed.filter((elem) => elem % 2 === 0);
+  console.log(result);
+}
+
+callFunc1(dataArr, doubler);
+callFunc1(dataArr, onlyEven);
+
+function callFunc2(data, cb) {
+  let str= Process.toJSON(data);
+  setTimeout(() => {
+    cb(str)
+  }, 2000);
+}
+
+function convertJ2(arg) {
+  console.log(arg);
+  console.log(typeof arg);
+}
+
+callFunc2(dataArr, convertJ2);
+
+
+
+
+
+
+
+
+
